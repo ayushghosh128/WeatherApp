eff09d2fcbc6f67730621b629bfb8f83ce5e7c74
ayushghosh128 <ayushghosh128@gmail.com>
2025-07-08 01:09:38 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FaSearch } from "react-icons/fa"
-import { useEffect, useState } from 'react'
+
+// ✅ Import images properly so Vite bundles them
+import cloudsIcon from './assets/clouds.png';
+import humidityIcon from './assets/humidity.png';
+import windIcon from './assets/wind.png';
+
 const App = () => {
   const [data, setData] = useState({
     celcius: 10,
@@ -10,16 +15,18 @@ const App = () => {
     speed: 3
   })
   const [name, setName] = useState('')
-  useEffect(() => {
-
-  }, [])
 
   const handleClick = () => {
     if (name !== "") {
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=82fa12dd6c070c70b0e9fc47f4912406&units=metric`;
       axios.get(apiUrl)
         .then(res => {
-          setData({ ...data, celcius: res.data.main.temp, name: res.data.name, humidity: res.data.main.humidity, speed: res.data.wind.speed })
+          setData({
+            celcius: res.data.main.temp,
+            name: res.data.name,
+            humidity: res.data.main.humidity,
+            speed: res.data.wind.speed
+          })
         })
         .catch(err => console.log(err));
     }
@@ -49,18 +56,20 @@ const App = () => {
         <div className='flex flex-col items-center'>
           <img
             className='w-32 sm:w-40 object-contain'
-            src="src/assets/clouds.png"
+            src={cloudsIcon}
             alt="Weather Icon"
           />
           <h1 className='text-3xl text-white font-semibold mt-2'>{data.celcius}°C </h1>
           <h2 className='text-2xl text-white font-semibold'>{data.name}</h2>
         </div>
+
+        {/* Humidity + Wind */}
         <div className='w-full flex justify-between'>
           <div className='flex flex-col items-center text-white'>
             <img
               className='w-16 sm:w-20 object-contain'
-              src="src\assets\humidity.png"
-              alt=""
+              src={humidityIcon}
+              alt="Humidity"
             />
             <p className='text-2xl font-bold'>{data.humidity}%</p>
             <p className='text-2xl font-bold'>Humidity</p>
@@ -68,8 +77,8 @@ const App = () => {
           <div className='flex flex-col items-center text-white'>
             <img
               className='w-16 sm:w-20 object-contain'
-              src="src\assets\wind.png"
-              alt=""
+              src={windIcon}
+              alt="Wind"
             />
             <p className='text-2xl font-bold '>{data.speed} km/h</p>
             <p className='text-2xl font-bold'>Wind Speed</p>
